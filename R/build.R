dab0cd44a7915a4e22ea8063e06f5b12e2af54f2
Will Hopper <wjhopper510@gmail.com>
2020-01-17 14:28:19 -0500

    add git-crypt encrypted lab solutions

--- R/build.R
+++ R/build.R
@@ -11,40 +11,49 @@ if ( !file.exists(output_dir) ) {
 }
 
 # Make collections dir if we need to
-if ( !file.exists(paste0("_", output_dir)) ) {
-  dir.create(paste0("_", output_dir))
+collections_dir <- file.path("..", "_labs")
+if ( !file.exists(collections_dir) ) {
+  dir.create(collections_dir)
 }
 
 counter <- 1
 for (lab in all_labs) {
   
+  ## Find and switch to lab files directory
   lab_dir <- file.path("..", 'labs_source', lab)
   old <- setwd(lab_dir)
-  rmd_files <- list.files(pattern = "*\\.Rmd$", full.names = TRUE)
   
+  ## Enumerate and render all Rmarkdown files
+  rmd_files <- list.files(pattern = "*\\.Rmd$", full.names = TRUE)
   for (file in rmd_files) {
     rmarkdown::render(file)
   }
   
+  # Switch back to old wd, since all our paths were relative to that directory.
   setwd(old)
+  
+  ## Copy the lab directory from the source to the site output folder
   file.copy(from=lab_dir, to=output_dir, recursive = TRUE)
-  
-  
-  ## Rename directory in numerical sequence
+
+  ## Rename the lab output directory in numerical sequence
   numerical_lab_dir <- file.path(output_dir, paste0("lab", counter))
   file.rename(from=file.path(output_dir, lab), to=numerical_lab_dir)
   
-  ## Rename lab Rmd template and assignment file
+  ## Remove solutions from site output
+  solutions <- list.files(numerical_lab_dir, pattern = "solution", full.names = TRUE)
+  file.remove(solutions)
+  
+  ## Rename lab Rmd template and assignment file to have informative names
   template <- list.files(file.path(numerical_lab_dir), pattern = "exercises.Rmd", full.names = TRUE, ignore.case = TRUE)
   assignment <- list.files(file.path(numerical_lab_dir), pattern = "exercises.html", full.names = TRUE, ignore.case = TRUE)
   file.rename(from=template, to=sub("exercises", paste0("Lab ", counter, " Exercises Template"), template, ignore.case = TRUE))
   file.rename(from=assignment, to=sub("exercises", paste0("Lab ", counter, " Exercises"), assignment, ignore.case = TRUE))
   
-  ## Rename slides
+  ## Rename slides to have informative names
   slides <- list.files(file.path(numerical_lab_dir), pattern =  "slides.*", full.names = TRUE, ignore.case = TRUE)
   file.rename(from=slides, to=sub("slides", lab, slides, ignore.case = TRUE))
   
-  ## Generate "post" for labs collection
+  ## Generate "post" with links for labs collection
   yaml_header <- paste0("---\n",
                         as.yaml(list(title = paste0("Lab ", counter, " - ", lab),
                                      author = "Will Hopper"
