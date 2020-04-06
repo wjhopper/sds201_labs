2926bbb564565accfd2513c987b636531983f54e
Will Hopper <wjhopper510@gmail.com>
2020-04-06 15:29:43 -0400

    added problem set 7 to the website

--- R/build.R
+++ R/build.R
@@ -2,6 +2,8 @@ library(yaml)
 library(rmarkdown)
 library(pander)
 
+root_dir <- normalizePath("..")
+
 all_labs <- yaml.load_file(file.path("..", "_config.yml"))$collections$labs$order
 
 ## Make static files dir if we need to
@@ -73,11 +75,21 @@ for (lab in all_labs) {
                                ),
                         "---\n"
                         )
-  body <- list(pandoc.link.return(paste0("labs/lab", counter, "/", lab, ".html"), paste0("Lab ", counter, " Slides")),
-               pandoc.link.return(paste0("labs/lab", counter, "/Lab ", counter, " Exercises.html"), paste0("Lab ", counter, " Exercises")),
-               pandoc.link.return(paste0("labs/lab", counter, "/Lab ", counter, " Exercises Template.Rmd"), paste0("Lab ", counter, " Exercises Template"))
+  
+  slides_final_path <- file.path("labs", paste0("lab", counter), paste0(lab, ".html"))
+  exercises_final_path <- file.path("labs", paste0("lab", counter), paste0("Lab ", counter, " Exercises.html"))
+  template_final_path <- file.path("labs", paste0("lab", counter), paste0("Lab ", counter, " Exercises Template.Rmd"))
+  
+  body <- list(pandoc.link.return(url = slides_final_path, text = paste0("Lab ", counter, " Slides")),
+               pandoc.link.return(url = exercises_final_path, text = paste0("Lab ", counter, " Exercises")),
+               pandoc.link.return(url = template_final_path, text = paste0("Lab ", counter, " Exercises Template"))
                )
-  body <- pandoc.list.return(body, add.end.of.list = FALSE)
+  
+  lab_file_exists <- vapply(file.path(root_dir, c(slides_final_path, exercises_final_path, template_final_path)),
+                            file.exists,
+                            logical(1)
+                            )
+  body <- pandoc.list.return(body[lab_file_exists], add.end.of.list = FALSE)
   
   post_file <- file.path("..", "_labs", paste0(lab, ".md"))
   if (file.exists(post_file)) {
---
+++ _labs/Multiple Regression.md
@@ -0,0 +1,7 @@
+---
+title: Lab 7 - Multiple Regression
+author: Will Hopper
+---
+
+* [Lab 7 Exercises](labs/lab7/Lab 7 Exercises.html) 
+* [Lab 7 Exercises Template](labs/lab7/Lab 7 Exercises Template.Rmd) 
